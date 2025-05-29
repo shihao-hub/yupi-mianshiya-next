0c6b50f883fd270179186a475a237a2166f6dfc7
shihao-hub <2958017271@qq.com>
2025-05-29 22:19:06 +0800

    Project initialization

---
+++ src/main/java/com/yupi/springbootinit/exception/ThrowUtils.java
@@ -0,0 +1,42 @@
+package com.yupi.springbootinit.exception;
+
+import com.yupi.springbootinit.common.ErrorCode;
+
+/**
+ * 抛异常工具类
+ */
+public class ThrowUtils {
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition 条件
+     * @param runtimeException RuntimeException
+     */
+    public static void throwIf(boolean condition, RuntimeException runtimeException) {
+        if (condition) {
+            throw runtimeException;
+        }
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition 条件
+     * @param errorCode ErrorCode
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode) {
+        throwIf(condition, new BusinessException(errorCode));
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition 条件
+     * @param errorCode ErrorCode
+     * @param message 异常信息
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode, String message) {
+        throwIf(condition, new BusinessException(errorCode, message));
+    }
+}
